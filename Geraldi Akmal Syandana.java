8dc7863c540b1eefd172c271864f6e298a9169fa
saya <geraldiakmal612@gmail.com>
2024-09-04 10:47:13 +0700

    upload biodata 1

---
+++ Geraldi Akmal Syandana.java
@@ -0,0 +1,20 @@
+/**
+ * Programtesting1
+ */
+public class Programtesting1 {
+    public static void main(String[] args){
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi Polinema --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Geraldi Akmal Syandana");
+        System.out.println("NIM     : 727777 ");
+        System.out.println("kelas   : TI 1B");
+        System.out.println("No Absen: 11   ");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Freddy Fazbar 6969");
+        System.out.println("No HP   : 081269696969");
+        System.out.println("Hobi    : Main Game");
+
+    }
+
+}
